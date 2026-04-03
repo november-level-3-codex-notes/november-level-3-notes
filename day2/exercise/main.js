c2e83a1b9e284605b2418b6d5d0b0a93df767890
Thomas Coons <tomcoons1413@gmail.com>
2026-04-03 19:17:52 -0400

    day2 notes added

---
+++ day2/exercise/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/***************************************/
+// module example below!
+import { functionOne, functionTwo } from "./someFileOne.js";
+import defaultFunction from "./someFileTwo.js";
+
+functionOne();
+functionTwo();
+defaultFunction();
+
+/***************************************/
+// // commonjs example below!
+// const func = require("./someFileThree.js");
+// func();
---
+++ day2/exercise/someFileOne.js
@@ -0,0 +1,8 @@
+"use strict";
+export function functionOne() {
+  console.log("Hello from function one!");
+}
+
+export function functionTwo() {
+  console.log("Hello from function two!");
+}
